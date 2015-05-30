b743c11a3cf3d2ea0a768dcb43aafc5228b4f453
luojie <luojie715@foxmail.com>
2015-05-30 23:08:36 +0800

    init

--- src/main/java/com/luojie/web/FirstServlet.java
+++ src/main/java/com/luojie/web/FirstServlet.java
@@ -17,6 +17,7 @@ public class FirstServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         int a;
+        int ok;
         response.getWriter().println("hello world servlet");
     }
 }
