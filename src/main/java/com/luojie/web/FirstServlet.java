b118ed9c75190086028361a80fea3fdf9b3b6ab7
luojie <luojie715@foxmail.com>
2015-05-30 22:59:45 +0800

    init

---
+++ src/main/java/com/luojie/web/FirstServlet.java
@@ -0,0 +1,21 @@
+package com.luojie.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by luojie on 15-5-30.
+ */
+public class FirstServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().println("hello world servlet");
+    }
+}
